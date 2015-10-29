c88471d3cad87153081db784ba699bb2502342ee
Jordi Jonkergouw <jordi_jkg@hotmail.com>
2015-10-29 11:55:18 +0100

    Bug Fix

--- week-1/twitter_test.py
+++ week-1/twitter_test.py
@@ -1,4 +1,4 @@
-import math
+from pattern.web import Twitter, plaintext
 
-a=math.ceil(math.pow(3,2)*19)
-print "Hello World, and welcome to CS ", int(a), "!"
+for tweet in Twitter().search('"more important than"', cached=False):
+	print plaintext(tweet.description)
